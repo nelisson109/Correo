8aa8c750c838b731cadd127bcc86f993adcb6d86
Nelisson109 <gavadianmartin@gmail.com>
2019-11-02 23:06:41 +0100

    probando treeview y correo scenebuilder

--- src/com/martin/Views/MainWindowController.java
+++ src/com/martin/Views/MainWindowController.java
@@ -1,4 +1,13 @@
 package com.martin.Views;
 
-public class MainWindowController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
